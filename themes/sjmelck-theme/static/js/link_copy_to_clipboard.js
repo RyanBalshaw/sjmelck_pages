6e050a44af9ae5b289dcca9abbe15832e04d9934
Ryan Balshaw <ryan@kriterion.ai>
2024-03-02 10:54:58 +0200

    Fixed link hovering. Ran linting.

--- themes/sjmelck-theme/static/js/link_copy_to_clipboard.js
+++ themes/sjmelck-theme/static/js/link_copy_to_clipboard.js
@@ -1,8 +1,21 @@
+// Function to copy the link to the clipboard
 function CopyLink(copyText) {
 
     // Copy the text inside the text field
     navigator.clipboard.writeText(copyText);
 
-    // Optional: display a message to the user
-    alert('Link copied to clipboard!');
+    // Show the toast that fades away
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastBootstrap.show()
+}
+
+// Functions to trigger the bootstrap toast element
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
 }
